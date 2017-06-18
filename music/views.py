9a5f231091daece9a293c0fa8f737479ca6d1b07
Erkanderon <erkan.cetinkaya3@gmail.com>
2017-06-18 12:44:09 +0300

    UpdateView and DeleteView is completed

--- music/views.py
+++ music/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Album
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 
 
@@ -16,4 +18,21 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
 	model = Album
 	template_name = "music/detail2.html"
+
+
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist','album_title','genre','album_logo']
+
+
+class AlbumUpdate(UpdateView):
+	model = Album
+	fields = ['artist','album_title','genre','album_logo']
+
+class AlbumDelete(DeleteView):
+	model = Album
+	success_url = reverse_lazy('music:index')
+
+
+
 
