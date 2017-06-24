4658b17c163556dad94f603c07c236a09b541a20
Erkanderon <erkan.cetinkaya3@gmail.com>
2017-06-24 18:00:20 +0300

    Register user completed

--- music/urls.py
+++ music/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/172
     url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
 
